0e6b159b77657cf7930873a94dca9f6bce47ab76
Salim Alam <salam@chef.io>
2015-10-30 12:29:07 -0700

    Initial CA framework

---
+++ CustomActionFastMsi/CustomAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace CustomActionFastMsi
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult FastUnzip(Session session)
+        {
+            session.Log("Begin CustomAction FastUnzip");
+
+            return ActionResult.Success;
+        }
+    }
+}
