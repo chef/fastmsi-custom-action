cf6c9b961fc27420a68bb4383a0b2163a008507c
Matt Wrock <matt@mattwrock.com>
2021-02-17 18:00:21 -0800

    call 7z.exe using absolute path
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- CustomActionFastMsi/CustomAction.cs
+++ CustomActionFastMsi/CustomAction.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Deployment.WindowsInstaller;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace CustomActionFastMsi
@@ -15,6 +16,7 @@ namespace CustomActionFastMsi
             mySession = session;
             session.Log("Starting FastUnzip");
 
+            string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string targetDir = session.CustomActionData["FASTZIPDIR"];
             string appName = session.CustomActionData["FASTZIPAPPNAME"];
 
@@ -39,13 +41,13 @@ namespace CustomActionFastMsi
             proc.StartInfo.CreateNoWindow = true;
             proc.StartInfo.RedirectStandardOutput = true;
             proc.StartInfo.RedirectStandardError = true;
-            proc.StartInfo.FileName = "7z.exe";
+            proc.StartInfo.FileName = Path.Combine(assemblyFolder , "7z.exe");
             proc.StartInfo.Arguments = string.Format("x {0} -aoa -o\"{1}\"", zipFile, Path.Combine(targetDir, appName));
             proc.EnableRaisingEvents = true;
             proc.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
             proc.ErrorDataReceived += new DataReceivedEventHandler(ErrorHandler);
 
-            session.Log("Starting 7zip extraction using arguments: " + proc.StartInfo.Arguments);
+            session.Log("Starting " + proc.StartInfo.FileName + " extraction using arguments: " + proc.StartInfo.Arguments);
 
             var stopWatch = new Stopwatch();
             stopWatch.Start();
